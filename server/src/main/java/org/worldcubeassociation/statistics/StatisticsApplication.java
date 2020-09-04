f75722dc806a44344281090a43e70810befb7cba
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-09-03 22:41:37 -0300

    Initial structure

---
+++ server/src/main/java/org/worldcubeassociation/statistics/StatisticsApplication.java
@@ -0,0 +1,13 @@
+package org.worldcubeassociation.statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticsApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/org/worldcubeassociation/statistics/StatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.worldcubeassociation.statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
