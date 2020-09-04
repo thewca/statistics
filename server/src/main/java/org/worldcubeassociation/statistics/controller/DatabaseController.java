d9abfffa21a25e8da33f3c4983469a89963e7c39
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-09-03 23:29:24 -0300

    Add database config

--- server/src/main/java/org/worldcubeassociation/statistics/controller/DatabaseController.java
+++ server/src/main/java/org/worldcubeassociation/statistics/controller/DatabaseController.java
@@ -5,16 +5,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.worldcubeassociation.statistics.service.DatabaseService;
 
 @RestController
+@RequestMapping("database")
 public class DatabaseController {
 	
 	@Autowired
 	private DatabaseService databaseService;
 	
-	@GetMapping("sql-query")
+	@GetMapping("query")
 	public List<LinkedHashMap<String, String>> getResultSet(String sqlQuery){
 		return databaseService.getResultSet(sqlQuery);
 	}
