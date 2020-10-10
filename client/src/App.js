cfc0b5a5aad929f00c136fe2471e6ad333eb02c5
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-10-10 10:55:58 -0300

    Add Bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App container-fluid">
-        <h1>Statistics</h1>
+        <h1 className="jumbotron">Statistics</h1>
       </div>
     );
   }
