b7c4b433761797beebd75bc78595367f2124ea1e
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2020-10-10 10:49:59 -0300

    Change Angular to React

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container-fluid">
+        <h1>Statistics</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
