a4028bbebfb002300d608e0041c83c057c332cf5
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2021-03-09 21:59:32 -0300

    Set default profile

--- server/src/main/java/org/worldcubeassociation/statistics/StatisticsApplication.java
+++ server/src/main/java/org/worldcubeassociation/statistics/StatisticsApplication.java
@@ -1,23 +1,22 @@
 package org.worldcubeassociation.statistics;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @SpringBootApplication
 public class StatisticsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(StatisticsApplication.class);
-		ConfigurableApplicationContext applicationContext = app.run(args);
-		Environment env = applicationContext.getEnvironment();
-		log.info("\n----------------------------------------------------------\n"
-				+ "\tAccess URL:\n\thttp://localhost:{}/swagger-ui.html\n"
-				+ "----------------------------------------------------------", env.getProperty("server.port"));
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(StatisticsApplication.class);
+        ConfigurableApplicationContext applicationContext = app.run(args);
+        Environment env = applicationContext.getEnvironment();
+        log.info("\n----------------------------------------------------------\n"
+                + "\tAccess URL:\n\thttp://localhost:{}/swagger-ui.html\n"
+                + "----------------------------------------------------------", env.getProperty("server.port"));
+    }
 
 }
