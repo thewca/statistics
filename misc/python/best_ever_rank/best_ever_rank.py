a7dc7c0e804dcd32c53b677dc95162b76773868d
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2021-04-11 22:24:06 -0300

    Add best rank end

--- misc/python/best_ever_rank/best_ever_rank.py
+++ misc/python/best_ever_rank/best_ever_rank.py
@@ -2,7 +2,7 @@
 
 import csv
 from bisect import bisect_left, insort_left
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # column order
 # ['competitionId', 'eventId', 'roundTypeId', 'pos', 'best', 'average', 'personName', 'personId', 'personCountryId', 'formatId', 'value1', 'value2', 'value3', 'value4', 'value5', 'regionalSingleRecord', 'regionalAverageRecord', 'year', 'month', 'day']
@@ -13,9 +13,11 @@ class Competitor:
     best = None
     best_rank = None
     best_rank_start = None
+    best_rank_end = None
 
-    def __init__(self, wca_id):
+    def __init__(self, wca_id, date):
         self.wca_id = wca_id
+        self.best_rank_start = date
 
     def __lt__(self, other):
         return self.wca_id < other.wca_id
@@ -24,10 +26,10 @@ class Competitor:
         return self.wca_id == other.wca_id
 
     def __repr__(self):
-        return "Competitor[wca_id=%s, best=%s, best_rank=%s]" % (self.wca_id, self.best, self.best_rank)
+        return "Competitor[wca_id=%s, best=%s, best_rank=%s, start=%s, end=%s]" % (self.wca_id, self.best, self.best_rank, self.best_rank_start, self.best_rank_end)
 
 
-def summarize_results(today_competitors, all_time_competitors, all_time_bests):
+def summarize_results(today, today_competitors, all_time_competitors, all_time_bests):
     # Assign today's best result
     for competitor in today_competitors:
         if not competitor.best:
@@ -53,6 +55,8 @@ def summarize_results(today_competitors, all_time_competitors, all_time_bests):
             insort_left(all_time_bests, competitor.best)
 
             all_time_competitors[index].best = competitor.best
+            all_time_competitors[index].best_rank_start = competitor.best_rank_start
+            all_time_competitors[index].best_rank_end = None
 
     for competitor in all_time_competitors:
         if not competitor.best:
@@ -61,6 +65,9 @@ def summarize_results(today_competitors, all_time_competitors, all_time_bests):
         current_best_rank = bisect_left(all_time_bests, competitor.best)
         if not competitor.best_rank or current_best_rank < competitor.best_rank:
             competitor.best_rank = current_best_rank
+
+        if current_best_rank > competitor.best_rank and not competitor.best_rank_end:
+            competitor.best_rank_end = today - timedelta(days=1)
 
 
 def main():
@@ -91,19 +98,19 @@ def main():
             if current_date != this_date:
                 # Compute rankings after today
 
-                summarize_results(today_competitors,
+                summarize_results(this_date, today_competitors,
                                   all_time_competitors, all_time_bests)
                 today_competitors = []
 
             wca_id = line[7]
-            competitor = Competitor(wca_id)
+            competitor = Competitor(wca_id, this_date)
 
             i = bisect_left(today_competitors, competitor)
             if i == len(today_competitors) or today_competitors[i] != competitor:
                 today_competitors.insert(i, competitor)
 
             competitor = today_competitors[i]
-            best = int(line[5])  # average because I know my best rank
+            best = int(line[4])  # average because I know my best rank
             old_best = competitor.best
             if best <= 0:
                 continue
@@ -111,7 +118,7 @@ def main():
                 # In this case, we should removeth old result and insert the new one
                 competitor.best = best
 
-        summarize_results(today_competitors,
+        summarize_results(current_date, today_competitors,
                           all_time_competitors, all_time_bests)
 
         c = 0
