8abfa3a4e1f037760d8434da80cbebe6974411f4
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2021-04-20 22:24:41 -0300

    Adapt best ever rank to use database

--- misc/python/best_ever_rank/best_ever_rank.py
+++ misc/python/best_ever_rank/best_ever_rank.py
@@ -1,40 +1,74 @@
 # python3 -m misc.python.best_ever_rank.best_ever_rank
 
-import csv
 from bisect import bisect_left, insort_left
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 
+from misc.python.model.competitor import Competitor as Comp
+from misc.python.util.database_util import get_database_connection
 from misc.python.util.log_util import log
-
-# column order
-# ['competitionId', 'eventId', 'roundTypeId', 'pos', 'best', 'average', 'personName', 'personId', 'personCountryId', 'formatId', 'value1', 'value2', 'value3', 'value4', 'value5', 'regionalSingleRecord', 'regionalAverageRecord', 'year', 'month', 'day']
 
 # WIP
 
+query_date = """set
+    @min_date = (
+        select
+            min(start_date)
+        from
+            Competitions c
+            inner join competition_events e on e.competition_id = c.id
+        where
+            start_date > %(date)s
+            and event_id = %(event_id)s
+    )"""
 
-class Competitor:
-    wca_id = None
-    best = None  # Global best
-    best_rank = None
-    best_rank_best = None  # Best considering the best rank
-    best_rank_start = None
-    best_rank_end = None
+query_next_results = """select
+    @min_date,
+    person_id,
+    min_best best,
+    (
+        select
+            competitionId
+        from
+            Results r
+            inner join Competitions c on r.competitionId = c.id
+        where
+            r.eventId = %(event_id)s
+            and r.best = min_best
+            and r.personId = personId
+            and c.start_date = @min_date
+        limit
+            1
+    ) competition_id
+from
+    (
+        select
+            personId person_id,
+            min(best) min_best
+        from
+            Results r
+            inner join Competitions c on r.competitionId = c.id
+        where
+            best > 0
+            and eventId = %(event_id)s
+            and c.start_date = @min_date
+        group by
+            personId
+    ) min_results"""
 
-    def __init__(self, wca_id, date):
-        self.wca_id = wca_id
-        self.best_rank_start = date
 
-    def __lt__(self, other):
-        return self.wca_id < other.wca_id
+class Competitor(Comp):
+    def __init__(self, wca_id, best, competition_id, min_date):
+        super().__init__(wca_id)
+        self.best = best
+        self.competition_id = competition_id
+        self.min_date = min_date
+        self.best_rank = None
 
-    def __eq__(self, other):
-        return self.wca_id == other.wca_id
-
-    def __repr__(self):
-        return "Competitor[wca_id=%s, best_rank_best=%s, best_rank=%s, start=%s, end=%s]" % (self.wca_id, self.best_rank_best, self.best_rank, self.best_rank_start, self.best_rank_end)
+    def __repr__(self) -> str:
+        return "Competitor[wca_id=%s, best=%s, competition_id=%s, min_date=%s, best_rank=%s]" % (self.wca_id, self.best, self.competition_id, self.min_date, self.best_rank)
 
 
-def summarize_results(today, today_competitors, all_time_competitors, all_time_bests):
+def summarize_results(today: date, today_competitors, all_time_competitors, all_time_bests):
     # Assign today's best result
     for competitor in today_competitors:
         index = bisect_left(all_time_competitors, competitor)
@@ -65,63 +99,50 @@ def summarize_results(today, today_competitors, all_time_competitors, all_time_b
         if not competitor.best_rank or current_best_rank < competitor.best_rank:
             competitor.best_rank = current_best_rank
             competitor.best_rank_best = competitor.best
+            competitor.best_rank_end = None
 
-        if current_best_rank > competitor.best_rank and not competitor.best_rank_end:
-            competitor.best_rank_end = today - timedelta(days=1)
+        # if current_best_rank > competitor.best_rank and not competitor.best_rank_end:
+        #     competitor.best_rank_end = today - timedelta(days=1)
 
 
 def main():
-    with open('WCA_export/WCA_export_Results_Ordered.tsv') as tsvin:
-        tsvin = csv.reader(tsvin, delimiter='\t')
 
-        all_time_bests = []
+    log.info("Get database connection")
+    cnx = get_database_connection()
+    cursor = cnx.cursor()
 
-        # Sorted by wca_id
-        all_time_competitors = []
-        today_competitors = []
+    all_time_bests = []
 
-        # Skip header
-        next(tsvin, None)
+    # Sorted by wca_id
+    all_time_competitors = []
+    today_competitors = []
 
-        current_date = None
-        for line in tsvin:
-            event = line[1]
-            best = int(line[5])  # average because I know my best rank
-            if event != '333fm' or best < 1:
-                continue
+    event_id = '333fm'
 
-            y, m, d = map(int, [line[17], line[18], line[19]])
-            this_date = datetime(y, m, d)
-            if current_date != this_date:
-                # Compute rankings after today
-                #log.info("Date %s", current_date)
-
-                summarize_results(this_date, today_competitors,
-                                  all_time_competitors, all_time_bests)
-                today_competitors = []
-                current_date = this_date
-
-            wca_id = line[7]
-            competitor = Competitor(wca_id, this_date)
-
-            i = bisect_left(today_competitors, competitor)
-            if i == len(today_competitors) or today_competitors[i] != competitor:
-                today_competitors.insert(i, competitor)
-
-            competitor = today_competitors[i]
-            old_best = competitor.best
-            if not old_best or best < old_best:
-                # In this case, we should removeth old result and insert the new one
-                competitor.best = best
+    log.info("Read results")
+    current_date = date(1970, 1, 1)
+    while True:
+        cursor.execute(
+            query_date, {"date": current_date, "event_id": event_id})
+        cursor.execute(query_next_results, {"event_id": event_id})
+        today_results = cursor.fetchall()
+        if not today_results:
+            break
+        for min_date, wca_id, best, competition_id in today_results:
+            competitor = Competitor(wca_id, best, competition_id, min_date)
+            today_competitors.append(competitor)
+            current_date = min_date
 
         # One last summarization for the last day
-        summarize_results(this_date, today_competitors,
+        summarize_results(current_date, today_competitors,
                           all_time_competitors, all_time_bests)
 
-        for competitor in all_time_competitors:
-            if competitor.wca_id == '2015CAMP17':
-                print(competitor)
-                break
+    for competitor in all_time_competitors:
+        if competitor.wca_id == '2015CAMP17':
+            print(competitor)
+            break
+
+    cnx.close()
 
 
 if __name__ == "__main__":
