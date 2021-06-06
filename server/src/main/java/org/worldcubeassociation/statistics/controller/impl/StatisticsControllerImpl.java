679fb5fdbfb8c03df064f27a33b281be6d4c9568
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2021-06-06 10:29:50 -0300

    Avoid using some endpoints in production

--- server/src/main/java/org/worldcubeassociation/statistics/controller/impl/StatisticsControllerImpl.java
+++ server/src/main/java/org/worldcubeassociation/statistics/controller/impl/StatisticsControllerImpl.java
@@ -1,33 +1,41 @@
 package org.worldcubeassociation.statistics.controller.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.worldcubeassociation.statistics.controller.StatisticsController;
 import org.worldcubeassociation.statistics.dto.StatisticsDTO;
 import org.worldcubeassociation.statistics.dto.StatisticsListDTO;
 import org.worldcubeassociation.statistics.dto.StatisticsRequestDTO;
 import org.worldcubeassociation.statistics.dto.StatisticsResponseDTO;
+import org.worldcubeassociation.statistics.exception.InvalidParameterException;
 import org.worldcubeassociation.statistics.service.StatisticsService;
 
-import java.io.IOException;
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 public class StatisticsControllerImpl implements StatisticsController {
     @Autowired
     private StatisticsService statisticsService;
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     public StatisticsResponseDTO sqlToStatistics(StatisticsRequestDTO statisticsRequestDTO) throws IOException {
+        validateProfile();
         return statisticsService.sqlToStatistics(statisticsRequestDTO);
     }
 
     @Override
     public void generateAllFromSql() throws IOException {
+        validateProfile();
         statisticsService.generateAllFromSql();
     }
 
     @Override
     public void generateFromSql(String filename) throws IOException {
+        validateProfile();
         statisticsService.generateFromSql(filename);
     }
 
@@ -43,11 +51,19 @@ public class StatisticsControllerImpl implements StatisticsController {
 
     @Override
     public StatisticsResponseDTO createStatistics(@Valid StatisticsDTO statisticsDTO) {
+        validateProfile();
         return statisticsService.create(statisticsDTO);
     }
 
     @Override
     public void deleteAll() {
+        validateProfile();
         statisticsService.deleteAll();
+    }
+
+    private void validateProfile() {
+        if ("prod".equals(profile)) {
+            throw new InvalidParameterException("This endpoint is not allowed in the environment " + profile);
+        }
     }
 }
