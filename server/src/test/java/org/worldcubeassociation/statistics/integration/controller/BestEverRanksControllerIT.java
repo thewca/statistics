8b9acb9f424fdc43afe9ca96e28748dacd4b0375
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-01-22 17:54:44 -0300

    Add test for generating all best ever ranks

--- server/src/test/java/org/worldcubeassociation/statistics/integration/controller/BestEverRanksControllerIT.java
+++ server/src/test/java/org/worldcubeassociation/statistics/integration/controller/BestEverRanksControllerIT.java
@@ -1,6 +1,9 @@
 package org.worldcubeassociation.statistics.integration.controller;
 
 import io.restassured.response.Response;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -12,11 +15,13 @@ import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.given;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @Sql({"/test-scripts/cleanTestData.sql", "/test-scripts/BestEverRanksControllerIT.sql"})
 public class BestEverRanksControllerIT extends AbstractTest {
     private static final String BASE_PATH = "/best-ever-rank/";
 
-    @MethodSource("autocompleteArguments")
+    @Order(1)
+    @MethodSource("listArguments")
     @ParameterizedTest(name = "index {0} status {1} wcaId {2} reason {3}")
     public void list(int index, HttpStatus status, String wcaId, String reason) {
         Response response = given()
@@ -31,10 +36,32 @@ public class BestEverRanksControllerIT extends AbstractTest {
         super.validateResponseIgnoreAttribute(index, response, "timestamp");
     }
 
-    static Stream<Arguments> autocompleteArguments() {
+    static Stream<Arguments> listArguments() {
         return Stream.of(
                 Arguments.of(0, HttpStatus.OK, "2015CAMP17", "Happy path for best ever rank"),
                 Arguments.of(1, HttpStatus.NOT_FOUND, "2022XXXX01", "User not found")
+        );
+    }
+
+    @Order(3)
+    @MethodSource("generateAllArguments")
+    @ParameterizedTest(name = "index {0} status {1} reason {2}")
+    public void generateAll(int index, HttpStatus status, String reason) {
+        Response response = given()
+                .spec(super.SPEC)
+                .when()
+                .post(BASE_PATH + "generate/all")
+                .then()
+                .statusCode(status.value())
+                .extract()
+                .response();
+
+        super.validateResponse(index, response);
+    }
+
+    static Stream<Arguments> generateAllArguments() {
+        return Stream.of(
+                Arguments.of(0, HttpStatus.OK, "Happy path")
         );
     }
 }
--- server/src/test/resources/test-scripts/cleanTestData.sql
+++ server/src/test/resources/test-scripts/cleanTestData.sql
@@ -1,1 +1,13 @@
 truncate wca_development.best_ever_rank;
+
+truncate wca_development.Competitions;
+
+truncate wca_development.competition_events;
+
+truncate wca_development.Results;
+
+truncate wca_development.Continents;
+
+truncate wca_development.Countries;
+
+truncate wca_development.Events;
