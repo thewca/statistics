12480f5e43243fbc1494e666e2e163d6c8fd7350
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-01-26 19:19:16 -0300

    Fix redirect uri

--- client/src/main/api/wca.api.ts
+++ client/src/main/api/wca.api.ts
@@ -5,7 +5,7 @@ export class WcaApi {
   login = () => {
     // We get the url from the server
     statisticsApi
-      .getWcaAuthenticationUrl(window.location.href)
+      .getWcaAuthenticationUrl(window.location.protocol + '//' + window.location.host)
       .then((response) => {
         // Navigante to the url
         window.location.href = response.data.url;
