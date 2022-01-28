b350780858c98b41b6052a4e664162ff11a5155b
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-01-28 18:48:25 -0300

    Add mbf result calculator

--- misc/python/util/time_util.py
+++ misc/python/util/time_util.py
@@ -1,15 +1,45 @@
-def time_format(cents, event=None, type=None):
-    if event == "333fm":
-        if type == "average":
-            return "%.2f" % (cents)
-        return cents
+def time_format(result, event=None, type=None):
+    if result == 0:
+        return '-'
 
-    cents = int(cents)
+    if result == -1:
+        return 'DNF'
+    if result == -2:
+        return 'DNF'
 
-    h, cents = divmod(cents, 360000)
-    m, cents = divmod(cents, 6000)
-    s, cents = divmod(cents, 100)
+    if event == '333fm':
+        if type == 'average':
+            return '%.2f' % (result/100)
+        return result
+
+    if event == '333mbf':
+        return mbld(str(result))
+
+    result = int(result)
+
+    h, result = divmod(result, 360000)
+    m, result = divmod(result, 6000)
+    s, result = divmod(result, 100)
 
     if h > 0:
-        return "%s:%s:%s.%s" % (h, str(m).zfill(2), str(s).zfill(2), str(cents).zfill(2))
-    return "%s:%s.%s" % (str(m).zfill(2), str(s).zfill(2), str(cents).zfill(2))
+        return '%s:%s:%s.%s' % (h, str(m).zfill(2), str(s).zfill(2), str(result).zfill(2))
+    if m > 0:
+        return '%s:%s.%s' % (m, str(s).zfill(2), str(result).zfill(2))
+    return '%s.%s' % (s, str(result).zfill(2))
+
+
+def mbld(result):
+    missed = int(result[-2:])
+    dd = int(result[:2])
+    difference = 99 - dd
+    solved = difference + missed
+    attempted = solved + missed
+    time_in_seconds = int(result[2:7])
+
+    h, time_in_seconds = divmod(time_in_seconds, 60*60)
+    m, s = divmod(time_in_seconds, 60)
+
+    time = f'{h}:' * (h > 0) + ('%s:%s' %
+                                (str(m).zfill(2), str(s).zfill(2)))
+
+    return f'{solved}/{attempted} {time}'
