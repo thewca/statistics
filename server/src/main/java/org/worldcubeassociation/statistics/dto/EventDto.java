2421e1c3b12f6cf5ffea47fe5940bf0f8670abdb
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-03-07 07:38:59 -0300

    Fix tests

--- server/src/main/java/org/worldcubeassociation/statistics/dto/EventDto.java
+++ server/src/main/java/org/worldcubeassociation/statistics/dto/EventDto.java
@@ -1,8 +1,10 @@
 package org.worldcubeassociation.statistics.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EventDto {
     private String id;
     private String name;
