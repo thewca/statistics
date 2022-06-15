28082ce0487d7812365f48a08ccc8c14001737f9
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-06-15 18:34:26 -0300

    add pagination capabilities

--- client/src/main/pages/SumOfRanks/SumOfRanksPage.tsx
+++ client/src/main/pages/SumOfRanks/SumOfRanksPage.tsx
@@ -1,7 +1,6 @@
-import { message, Select, Skeleton, Tooltip } from "antd";
+import { message, Pagination, Select, Skeleton, Space, Tooltip } from "antd";
 import { useCallback, useEffect, useState } from "react";
 import { sumOfRanksApi } from "../../api/SumOfRanksApi";
-import WcaEvent from "../../model/Event";
 import { MetaSorInfo } from "../../model/rank/MetaSorInfo";
 import { SumOfRanks } from "../../model/rank/SumOfRanks";
 import styles from "./SumOfRanksPage.module.css";
@@ -12,12 +11,15 @@ const INITIAL_PAGE_SIZE = 20;
 
 export const SumOfRanksPage = () => {
   const [page, setPage] = useState(0);
-  const [size, setSize] = useState(INITIAL_PAGE_SIZE);
+  const [pageSize, setPageSize] = useState(INITIAL_PAGE_SIZE);
+  const [totalSize, setTotalSize] = useState<number>();
   const [ranks, setRanks] = useState<SumOfRanks[]>([]);
   const [loading, setLoading] = useState(false);
   const [metaSor, setMetaSor] = useState<MetaSorInfo[]>([]);
   const [resultType, setResultType] = useState<string>();
   const [selectedRegionGroup, setSelectedRegionGroup] = useState<string>();
+  const [regionType, setRegionType] = useState<string>();
+  const [region, setRegion] = useState<string>();
 
   const fetchMetaInfor = useCallback(() => {
     sumOfRanksApi
@@ -36,17 +38,19 @@ export const SumOfRanksPage = () => {
 
   const fetchSumOfRanks = useCallback(
     (
-      region: string,
-      regionType: string,
       resultType: string,
+      regionType: string,
+      region: string,
       page: number,
       pageSize: number
     ) => {
       setLoading(true);
       sumOfRanksApi
-        .listSumOfRanks(region, regionType, resultType, page, pageSize)
+        .listSumOfRanks(resultType, regionType, region, page, pageSize)
         .then((response) => {
           setRanks(response.data);
+          setPage(page);
+          setPageSize(pageSize);
         })
         .finally(() => setLoading(false));
     },
@@ -59,91 +63,121 @@ export const SumOfRanksPage = () => {
     if (!resultType || !selectedRegionGroup) {
       return;
     }
-    setRanks([]);
     let [regionType, region] = selectedRegionGroup.split("-");
+    setRegion(region);
+    setRegionType(regionType);
+
+    let newTotal = metaSor
+      .find((m) => m.resultType === resultType)
+      ?.regionGroups.find((r) => r.regionType === regionType)
+      ?.regions.find((r) => r.region === region)?.totalSize;
+    setTotalSize(newTotal);
 
     fetchSumOfRanks(resultType, regionType, region, 0, INITIAL_PAGE_SIZE);
-  }, [resultType, selectedRegionGroup, fetchSumOfRanks]);
+  }, [metaSor, resultType, selectedRegionGroup, fetchSumOfRanks]);
 
   return (
     <>
       <h1 className="page-title">Sum of Ranks</h1>
-
-      <Select
-        className={styles.regionTypes}
-        value={resultType}
-        onChange={(e) => setResultType(e)}
-        options={metaSor.map((r) => ({
-          label: r.resultType,
-          value: r.resultType,
-        }))}
-      />
-      <Select
-        className={styles.regionTypes}
-        value={selectedRegionGroup}
-        onChange={setSelectedRegionGroup}
-        optionFilterProp="children"
-        showSearch
-      >
-        {metaSor
-          .find((r) => r.resultType === resultType)
-          ?.regionGroups.map((g) => (
-            <OptGroup key={g.regionType}>
-              {g.regions.map((r) => (
-                <Option key={r.region} value={`${g.regionType}-${r.region}`}>
-                  {r.region}
-                </Option>
-              ))}
-            </OptGroup>
-          ))}
-      </Select>
-      <Skeleton loading={loading} />
-
-      {ranks.length > 0 && (
-        <div className={styles.ranksTable}>
-          <table>
-            <thead>
-              <tr>
-                <th>Rank</th>
-                <th>Person</th>
-                <th>Overall</th>
-                {metaSor
-                  .find((m) => m.resultType === resultType)
-                  ?.availableEvents.map((e) => (
-                    <th key={e.id}>
-                      <Tooltip title={e.name}>
-                        <span className={`cubing-icon event-${e.id}`} />
-                      </Tooltip>
-                    </th>
-                  ))}
-              </tr>
-            </thead>
-            <tbody>
-              {ranks.map((r) => (
-                <tr key={r.wcaId}>
-                  <th>{r.regionRank}</th>
-                  <td>{r.wcaId}</td>
-                  <td>{r.overall}</td>
-                  {r.events.map((e) => (
-                    <td
-                      key={e.eventId}
-                      className={
-                        !e.completed
-                          ? styles.notCompleted
-                          : e.regionalRank <= 10
-                          ? styles.top10
-                          : ""
-                      }
+      <Space direction="vertical" className={styles.contentContainer}>
+        <div>
+          <Select
+            className={styles.regionTypes}
+            value={resultType}
+            onChange={setResultType}
+            options={metaSor.map((r) => ({
+              label: r.resultType,
+              value: r.resultType,
+            }))}
+          />
+          <Select
+            className={styles.regionTypes}
+            value={selectedRegionGroup}
+            onChange={setSelectedRegionGroup}
+            optionFilterProp="children"
+            showSearch
+          >
+            {metaSor
+              .find((r) => r.resultType === resultType)
+              ?.regionGroups.map((g) => (
+                <OptGroup key={g.regionType}>
+                  {g.regions.map((r) => (
+                    <Option
+                      key={r.region}
+                      value={`${g.regionType}-${r.region}`}
                     >
-                      {e.regionalRank}
-                    </td>
+                      {r.region}
+                    </Option>
                   ))}
-                </tr>
+                </OptGroup>
               ))}
-            </tbody>
-          </table>
+          </Select>
         </div>
-      )}
+        <Skeleton loading={loading} />
+
+        {!loading && ranks.length > 0 && (
+          <>
+            <div>
+              <Pagination
+                total={totalSize}
+                pageSize={pageSize}
+                current={page + 1}
+                showQuickJumper
+                onChange={(p, s) => {
+                  fetchSumOfRanks(resultType!, regionType!, region!, p - 1, s);
+                }}
+              />
+            </div>
+            <div className={styles.ranksTable}>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Rank</th>
+                    <th>Person</th>
+                    <th>Overall</th>
+                    {metaSor
+                      .find((m) => m.resultType === resultType)
+                      ?.availableEvents.map((e) => (
+                        <th key={e.id}>
+                          <Tooltip title={e.name}>
+                            <span className={`cubing-icon event-${e.id}`} />
+                          </Tooltip>
+                        </th>
+                      ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {ranks.map((r, i) => (
+                    <tr key={r.wcaId}>
+                      <th>
+                        {i === 0 || ranks[i].overall !== ranks[i - 1].overall
+                          ? r.regionRank
+                          : "-"}
+                      </th>
+                      <td>{r.wcaId}</td>
+                      <td>{r.overall}</td>
+                      {r.events.map((e, i) => (
+                        <td
+                          key={i}
+                          className={
+                            !e.completed
+                              ? styles.notCompleted
+                              : e.regionalRank <= 10
+                              ? styles.top10
+                              : ""
+                          }
+                        >
+                          {e.regionalRank}
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </>
+        )}
+      </Space>
     </>
   );
 };
