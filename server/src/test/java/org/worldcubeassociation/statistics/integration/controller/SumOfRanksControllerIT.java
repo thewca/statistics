8e168b22d1cbe22c12ab878c3bc6728b110643cb
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2022-06-16 15:50:32 -0300

    Add more SOR test cases

--- server/src/test/java/org/worldcubeassociation/statistics/integration/controller/SumOfRanksControllerIT.java
+++ server/src/test/java/org/worldcubeassociation/statistics/integration/controller/SumOfRanksControllerIT.java
@@ -78,7 +78,37 @@ public class SumOfRanksControllerIT extends AbstractTest {
                 Arguments.of(1, HttpStatus.OK, "Single", "World", "World", Map.of("page", 1, "pageSize", 6),
                         "Another pagination success"),
                 Arguments.of(2, HttpStatus.BAD_REQUEST, "Single", "World", "World", Map.of("page", 0, "pageSize", 120),
-                        "Invalid pagination")
+                        "Invalid pagination"),
+                Arguments.of(3, HttpStatus.OK, "Single", "World", "World",
+                        Map.of("page", 0, "pageSize", 3, "wcaId", "2017SOUZ10"),
+                        "Search by wca id"),
+                Arguments.of(4, HttpStatus.OK, "Average", "Continent", "South America",
+                        Map.of("page", 0, "pageSize", 3, "wcaId", "2017SOUZ10"),
+                        "Search by wca id, continent"),
+                Arguments.of(5, HttpStatus.OK, "Average", "Country", "Brazil",
+                        Map.of("page", 0, "pageSize", 3, "wcaId", "2017SOUZ10"),
+                        "Search by wca id, country"),
+                Arguments.of(6, HttpStatus.BAD_REQUEST, "Average", "Country", "Brazil",
+                        Map.of("page", 0, "pageSize", 3, "wcaId", "2015CAMP17"),
+                        "User not found for region")
         );
+    }
+
+    @Test
+    @Order(3)
+    public void meta() {
+        // Test scripts clears database everytime
+        generateSor();
+
+        Response response = given()
+                .spec(super.SPEC)
+                .when()
+                .get(BASE_PATH + "meta")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .response();
+
+        super.validateResponse(0, response);
     }
 }
