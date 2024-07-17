5bd6b0c0b83fc93f8114dee53b7ce2f3f4de4c9f
Alexandre Henrique Afonso Campos <camposalexandreh@gmail.com>
2024-07-17 00:30:40 -0300

    Remove unused endpoint for authantication (FE handles it)

--- server/src/main/java/org/worldcubeassociation/statistics/service/impl/WCAServiceImpl.java
+++ server/src/main/java/org/worldcubeassociation/statistics/service/impl/WCAServiceImpl.java
@@ -5,30 +5,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.worldcubeassociation.statistics.api.WCAApi;
 import org.worldcubeassociation.statistics.dto.UserInfoDTO;
-import org.worldcubeassociation.statistics.response.AuthenticationResponse;
 import org.worldcubeassociation.statistics.service.WCAService;
 
 @Service
 public class WCAServiceImpl implements WCAService {
+
     @Value("${api.wca.baseurl}")
     private String wcaBaseUrl;
-
-    @Value("${api.wca.appid}")
-    private String wcaAppId;
 
     @Autowired
     private WCAApi wcaApi;
 
     @Override
-    public AuthenticationResponse getWcaAuthenticationUrl(String frontendHost, String redirect) {
-        return AuthenticationResponse.builder().frontendHost(frontendHost).url(String
-                .format("%s/oauth/authorize?client_id=%s&redirect_uri=%s?redirect=%s&response_type=token&scope=public", wcaBaseUrl,
-                        wcaAppId, frontendHost, redirect)).build();
-    }
-
-    @Override
     public UserInfoDTO getUserInfo(String token) {
         return wcaApi.getUserInfo(token);
     }
-
 }
